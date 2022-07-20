3493b8434e64d0086c2bc7a6695b469ef6e08546
Manish <manishe6@gmail.com>
2022-07-20 10:23:00 +0530

    changes

---
+++ Documents/javafsd/assistedproject/assitedproject/src/typecasting.java
@@ -0,0 +1,24 @@
+//implicit
+public class typecasting {
+	public static void main(String[] args) {
+		int i = 10;
+        long l = i;
+        float f = l;
+        System.out.println("Int value " + i);
+        System.out.println("Long value " + l);
+        System.out.println("Float value " + f);
+    
+
+	
+	
+//explicit
+
+		long z = 100;
+		int a = (int)l;
+		System.out.println("Long value " + z);
+		System.out.println("Int value " + a);
+}
+}
+
+
+
